1d00f8a8fe97bea31f095e0aaee977da8253c422
deanbred <deanbred@gmail.com>
2023-05-25 02:56:38 -0400

    new build

--- out/_next/static/986CAkWTaN2-Ixk9Q0s0f/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/343-8200e9e9bd608fab.js","static/css/09934c8e63573379.css","static/chunks/pages/index-b6cd7efea2dcfdbf.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
