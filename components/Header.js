a7ef5473f005225d8346872c138b657c233a2ae7
deanbred <deanbred@gmail.com>
2023-06-01 16:00:39 -0400

    always on top

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function Header() {
   return (
-    <nav className="p-3 flex flex-row justify-center sticky top-0 bg-gradient-to-r from-[#b8a57e] to-[#7a6b54] border-b-2 overflow-hidden">
+    <nav className="p-3 z-10 flex flex-row justify-center sticky top-0  bg-gradient-to-r from-[#b8a57e] to-[#7a6b54] border-b-2 overflow-hidden">
       <div className="mr-auto">
         <Image src="logo-00.jpg" width={72} height={72} alt="Logo"></Image>
       </div>
