5c3a2ef5e76bf73a35c280a98b3721a347d10622
deanbred <deanbred@gmail.com>
2023-07-04 10:42:01 -0400

    header colors

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function Header() {
   return (
-    <nav className="p-3 z-10 flex flex-row justify-center sticky top-0 bg-gradient-to-r from-[#bfe0e2] to-[#283a4c] border-b-2 overflow-hidden">
+    <nav className="p-3 z-10 flex flex-row justify-center sticky top-0 bg-gradient-to-r from-[#cde5e5] to-[#376578] border-b-2 overflow-hidden">
       <div className="mr-auto">
         <Image src="logo-00.jpg" width={72} height={72} alt="Logo"></Image>
       </div>
