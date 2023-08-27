abb7527dd3d99dc78334796b18326aa13dbc6a62
deanbred <deanbred@gmail.com>
2023-08-27 08:32:10 -0400

    deploy

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import Image from "next/image"
 export default function Header() {
   return (
     <nav className="z-10 font-head font-bold flex flex-row justify-center sticky top-0 bg-gradient-to-r from-[#a9ba73] to-[#1b3d3f] border-b-2 overflow-hidden">
-        <h2 className="m-auto text-[#cf4230] lg:text-5xl md:text-4xl sm:text-3xl text-2xl">
+        <h2 className="m-auto text-[#cf4230] lg:text-5xl md:text-4xl sm:text-3xl text-3xl">
           Anti-MEV
         </h2>
         <div className="p-2 mr-2">
